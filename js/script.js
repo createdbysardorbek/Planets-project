295e5f92e7c9628bde4487e00cf77f13765b8b85
Sardor <cr720051010@gmail.com>
2022-12-27 18:27:28 +0300

    find clicked object in json

--- js/script.js
+++ js/script.js
@@ -3,6 +3,9 @@ console.log(nav);
 
 nav.addEventListener('click', (e) => {
     if (e.target.dataset.planet) {
-
+        fetch('planets.json').then(res => res.json()).then(data => {
+            let planet = data.planets.find(elem => elem.name === e.target.dataset.planet);
+            console.log(planet);
+        })
     }
 })
